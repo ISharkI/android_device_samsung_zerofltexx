d3cb291626e1254954e2bccb613f865d78231ffe
Usaamah Patel <ussyp2015@gmail.com>
2017-03-06 17:15:24 +0000

    Switch to LineageOS

--- lineage.mk
+++ lineage.mk
@@ -5,13 +5,13 @@ TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="Galaxy S6" \
     TARGET_DEVICE="Galaxy S6"
 
-PRODUCT_NAME := aokp_zerofltexx
+PRODUCT_NAME := lineage_zerofltexx
 PRODUCT_DEVICE := zerofltexx
 
 TARGET_UNOFFICIAL_BUILD_ID := Nexus7420
